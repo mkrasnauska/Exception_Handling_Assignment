f67403d30004b9fe0e9208ed3a521beb0a38915f
Margarita <margaritakrasnauska@gmail.com>
2023-02-22 12:08:22 +0000

    Add project files.

---
+++ Exception_Handling_Assignment/Exceptions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling_Assignment
+{
+    public class Exceptions
+    {
+        public class NegativeException : Exception
+        {
+            public NegativeException()
+                : base() { }
+            public NegativeException(string message)
+                : base(message) { }
+        }
+        public class TooBigException : Exception
+        {
+            public TooBigException()
+                : base() { }
+            public TooBigException(string message)
+                : base(message) { }
+        }
+        public class ZeroException : Exception
+        {
+            public ZeroException()
+                : base() { }
+            public ZeroException(string message)
+                : base(message) { }
+        }
+    }
+}
+
---
+++ Exception_Handling_Assignment/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling_Assignment
+{
+    internal class Program
+    {
+        static void Main()
+
+        {
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("===============Welcome===============");
+            try
+            {
+                bool ValidAge = false;
+                int YourAge = 0;
+                while (!ValidAge)
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Please type in your age");
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    ValidAge = int.TryParse(Console.ReadLine(), out YourAge);
+                    if (!ValidAge) throw new Exception();
+
+                }
+                if (YourAge == 0)
+                {
+                    throw new Exceptions.ZeroException();
+                }
+                if (YourAge < 0)
+                {
+                    throw new Exceptions.NegativeException();
+                }
+                if (YourAge > 150)
+                {
+                    throw new Exceptions.TooBigException();
+                }
+
+                string CurrentDate = DateTime.Now.ToString("yyyy");
+                Double DateOfBirth = Convert.ToDouble(CurrentDate) - YourAge;
+                Console.WriteLine("You are " + YourAge + " years old.\nYour Date Of Birth is : " + DateOfBirth);
+            }
+
+            catch (Exceptions.NegativeException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Positive Values only\n No Zeroes");
+                Console.ReadLine();
+
+
+            }
+            catch (Exceptions.ZeroException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Your age can't be zero");
+                Console.ReadLine();
+
+
+            }
+            catch (Exceptions.TooBigException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("That is not possible!!!Are you a vampire?");
+                Console.ReadLine();
+
+            }
+            catch (Exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error!Please check your values!");
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("===============GoodBye===============");
+            Console.ReadLine();
+        }
+    }
+}
+
